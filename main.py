33f1367656a5393b145f0bc27392e723639340d2
Yeetnuggets <bananazzzzz020@gmail.com>
2023-01-25 16:43:04 -0700

    i fixa da thing

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ layout = [[sg.Input(key='in', size=31)],
           [sg.ReadFormButton('.'), sg.ReadFormButton('0'), sg.ReadFormButton('='), sg.ReadFormButton('/')]
           ]
 
-window = sg.Window('Calculator', default_button_element_size = (5, 2), auto_size_buttons = False, grab_anywhere = False, resizable=False, use_custom_titlebar=True, titlebar_background_color='black', titlebar_text_color='white', icon=r'C:\Users\meyee\Desktop\A Fucking Calculator\icon.ico')
+window = sg.Window('Calculator', default_button_element_size = (5, 2), auto_size_buttons = False, grab_anywhere = False, resizable=False, use_custom_titlebar=True, titlebar_background_color='black', titlebar_text_color='white', titlebar_icon=f'{desk}\A-Fucking-Calculator-With-a-GUI\calcicon.png')
 window.layout(layout)
 
 current_num = []
@@ -34,53 +34,56 @@ full_operation = []
 
 
 while True:
-    button, values = window.read()
+    try:
+        button, values = window.read()
 
-    if button == sg.WINDOW_CLOSED:
-        break
+        if button == sg.WINDOW_CLOSED:
+            break
 
-    if button in ['0', '1', '2', '3', '4', '5', '6', '7' , '8', '9', '.']:
-        current_num.append(button)
-        num_string = ''.join(current_num)
-        window['in'].update(num_string)
-
-
-    if button in ['+', '-', '*', '/']:
-        full_operation.append(''.join(current_num))
-        current_num = []
-        e = full_operation.append(button)
-        window['in'].update(e)
+        if button in ['0', '1', '2', '3', '4', '5', '6', '7' , '8', '9', '.']:
+            current_num.append(button)
+            num_string = ''.join(current_num)
+            window['in'].update(num_string)
 
 
-    if button == '=':
-        full_operation.append(''.join(current_num))
-        result = eval(''.join(full_operation))
-        window['in'].update(result)
-        full_operation = []
+        if button in ['+', '-', '*', '/']:
+            full_operation.append(''.join(current_num))
+            current_num = []
+            e = full_operation.append(button)
+            window['in'].update(e)
 
 
-    elif button == 'C':
-        full_operation = []
-        current_num = []
-        window['in'].update('')
+        if button == '=':
+            full_operation.append(''.join(current_num))
+            result = eval(''.join(full_operation))
+            window['in'].update(result)
+            full_operation = []
 
 
-    elif button == '√':
-        sqr = values['in']
-        sqr = float(sqr)
-        window['in'].update(float(sqrt(sqr)))
-        full_operation = []
+        elif button == 'C':
+            full_operation = []
+            current_num = []
+            window['in'].update('')
 
-    elif button == 'sin(':
-        sine = values['in']
-        sine = float(sine)
-        window['in'].update(sin(sine))
 
-    elif button == '**':
-        full_operation.append(''.join(current_num))
-        current_num = []
-        e = full_operation.append(button)
-        window['in'].update(e)
+        elif button == '√':
+            sqr = values['in']
+            sqr = float(sqr)
+            window['in'].update(float(sqrt(sqr)))
+            full_operation = []
+
+        elif button == 'sin(':
+            sine = values['in']
+            sine = float(sine)
+            window['in'].update(sin(sine))
+
+        elif button == '**':
+            full_operation.append(''.join(current_num))
+            current_num = []
+            e = full_operation.append(button)
+            window['in'].update(e)
+    except Exception as e:
+        sg.popup_error(e)
 
 
 
