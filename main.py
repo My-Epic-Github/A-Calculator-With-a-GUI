9cc79a03b7e77117ea50df79c3b2922140175339
Yeetnuggets <bananazzzzz020@gmail.com>
2023-01-26 14:29:38 -0700

    yeah

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ def sin(args):
 
 desk = winshell.desktop()
 layout = [[sg.Input(key='in', size=31)],
-          [sg.ReadFormButton('C'), sg.ReadFormButton('√'), sg.ReadFormButton('sin('), sg.ReadFormButton('**')],
+          [sg.ReadFormButton('C'), sg.ReadFormButton('√'), sg.ReadFormButton('x^2'), sg.ReadFormButton('**')],
           [sg.ReadFormButton('1'), sg.ReadFormButton('2'), sg.ReadFormButton('3'), sg.ReadFormButton('+')],
           [sg.ReadFormButton('4'), sg.ReadFormButton('5'), sg.ReadFormButton('6'), sg.ReadFormButton('-')],
           [sg.ReadFormButton('7'), sg.ReadFormButton('8'), sg.ReadFormButton('9'), sg.ReadFormButton('*')],
           [sg.ReadFormButton('.'), sg.ReadFormButton('0'), sg.ReadFormButton('='), sg.ReadFormButton('/')]
           ]
 
-window = sg.Window('Calculator', default_button_element_size = (5, 2), auto_size_buttons = False, grab_anywhere = False, resizable=False, use_custom_titlebar=True, titlebar_background_color='black', titlebar_text_color='white', titlebar_icon=f'{desk}\A-Fucking-Calculator-With-a-GUI\calcicon.png')
+window = sg.Window('Calculator', default_button_element_size = (5, 2), auto_size_buttons = False, grab_anywhere = False, resizable=False, use_custom_titlebar=True, titlebar_background_color='black', titlebar_text_color='white', titlebar_icon=output.icon)
 window.layout(layout)
 
 current_num = []
@@ -72,10 +72,10 @@ while True:
             window['in'].update(float(sqrt(sqr)))
             full_operation = []
 
-        elif button == 'sin(':
-            sine = values['in']
-            sine = float(sine)
-            window['in'].update(sin(sine))
+        elif button == 'x^2':
+            srq = values['in']
+            srq = float(srq)
+            window['in'].update(srq**2)
 
         elif button == '**':
             full_operation.append(''.join(current_num))
